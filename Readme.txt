5229db3d06269fcf945a8991c2211c1275bc811a
frank wester <wester.frank@hotmail.com>
2020-05-18 11:45:33 +0200

    updated swagger stub

--- Readme.txt
+++
@@ -1,6 +0,0 @@
-Dit is de repostory van groep 5 voor project Code Generatie 2.4
-
-Gentle Possel
-Frank Wester 
-Sander Boeree
-Thomas Wubben
---
+++ spring-server-generated (4)/src/main/java/io/swagger/model/ApiKey.java
@@ -0,0 +1,78 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * ApiKey
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-05-18T09:28:40.437Z[GMT]")
+public class ApiKey   {
+  @JsonProperty("apiKey")
+  private String apiKey = null;
+
+  public ApiKey apiKey(String apiKey) {
+    this.apiKey = apiKey;
+    return this;
+  }
+
+  /**
+   * Get apiKey
+   * @return apiKey
+  **/
+  @ApiModelProperty(example = "abcdef12345", value = "")
+  
+    public String getApiKey() {
+    return apiKey;
+  }
+
+  public void setApiKey(String apiKey) {
+    this.apiKey = apiKey;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ApiKey apiKey = (ApiKey) o;
+    return Objects.equals(this.apiKey, apiKey.apiKey);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(apiKey);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ApiKey {\n");
+    
+    sb.append("    apiKey: ").append(toIndentedString(apiKey)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
